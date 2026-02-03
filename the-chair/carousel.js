60a65a2870bcc6f3a5ea10c04d2eb1a4e335843c
kiki <142045045+calciferchang@users.noreply.github.com>
2026-02-03 00:26:04 -0500

    configure swiper options

--- the-chair/carousel.js
+++ the-chair/carousel.js
@@ -1,19 +1,27 @@
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.mjs'
-import 'swiper/css/bundle'
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.mjs";
+import "swiper/css/bundle";
 
 const swiperV = new Swiper(".swiper-container-v", {
   direction: "vertical",
-  spaceBetween: 50,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+  grabCursor: true,
+  keyboard: {
+    enabled: true,
+  },
+  loop: true,
+  mousewheel: {
+    enabled: true,
+    forceToAxis: true,
   },
 });
 
 const swiperH = new Swiper(".swiper-container-h", {
-  spaceBetween: 50,
-  pagination: {
-    el: ".swiper-pagination-h",
-    clickable: true,
+  grabCursor: true,
+  keyboard: {
+    enabled: true,
+  },
+  loop: true,
+  mousewheel: {
+    enabled: true,
+    forceToAxis: true,
   },
 });
