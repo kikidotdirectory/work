ba71b8abea214a731274039e979d1588210e7e0c
kiki <142045045+calciferchang@users.noreply.github.com>
2026-02-05 14:53:14 -0500

    passthrough svgs

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -4,6 +4,7 @@ import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("**/*.css");
   eleventyConfig.addPassthroughCopy("**/*.js");
+  eleventyConfig.addPassthroughCopy("**/*.svg");
   eleventyConfig.addPlugin(EleventyVitePlugin);
   eleventyConfig.addPlugin(eleventyImageTransformPlugin);
 };
