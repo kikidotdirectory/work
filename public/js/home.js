17b743aa3acf910ec7a525ef4f2d42b84c4f1ae2
kiki <142045045+calciferchang@users.noreply.github.com>
2026-02-11 12:04:59 -0500

    use simpler function for chair-thumbnail

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,14 @@
-const chairThumbnail = new Swiper('.chair-thumbnail', {
-  loop: true,
-})
+const thumbnails = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png'];
+let current = 0;
+
+// Preload next image
+function preloadNext() {
+  const next = new Image();
+  next.src = thumbnails[(current + 1) % thumbnails.length];
+}
+
+setInterval(() => {
+  current = (current + 1) % thumbnails.length;
+  document.getElementById('chair-thumbnail').src = 'assets/thumbnails/the-chair/'+thumbnails[current];
+  preloadNext();
+}, 3000);
