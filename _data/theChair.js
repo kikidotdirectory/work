2e07d96820ee0a9483155fa8445c8ebff075e4ae
kiki <142045045+calciferchang@users.noreply.github.com>
2026-02-11 17:27:32 -0500

    cleanup

--- _data/theChair.js
+++ _data/theChair.js
@@ -4,7 +4,9 @@ import { promises as fs } from "fs";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const PROJECT_ROOT = path.join(__dirname, "..");
+// assets dir = public/assets/the-chair/
 const ASSETS_DIR = path.join(PROJECT_ROOT, "public","assets", "the-chair");
 const assetsContents = await fs.readdir(ASSETS_DIR, { withFileTypes: true });
 const directories = assetsContents.filter((fileEntry) =>
