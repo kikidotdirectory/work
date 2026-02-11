3f7cc8979854e923998306c606c7049381761ba5
kiki <142045045+calciferchang@users.noreply.github.com>
2026-02-10 19:00:38 -0500

    restructure project

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,17 +1,37 @@
 import EleventyVitePlugin from "@11ty/eleventy-plugin-vite";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
-export const config = {
-  dir: {
-    input: "src",
-    output: "_site",
-  }
-}
-
-export default function (eleventyConfig) {
+export default async function (eleventyConfig) {
+  eleventyConfig
+    .addPassthroughCopy({
+      "./public/": "/"
+    })
+    .addPassthroughCopy("**/*.png")
   eleventyConfig.addPassthroughCopy("**/*.css");
   eleventyConfig.addPassthroughCopy("**/*.js");
   eleventyConfig.addPassthroughCopy("**/*.svg");
-  eleventyConfig.addPlugin(EleventyVitePlugin);
-  eleventyConfig.addPlugin(eleventyImageTransformPlugin);
-};
+
+  // enable smart quotes
+  eleventyConfig.amendLibrary("md", function (md) {
+    md.set({
+      typographer: true,
+    });
+
+    // plugins
+    eleventyConfig.addPlugin(EleventyVitePlugin);
+    eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+  });
+}
+
+export const config = {
+  markdownTemplateEngine: "njk",
+  htmlTemplateEngine: "njk",
+  dir: {
+    input: "content",
+    includes: "../_includes",
+    layouts: "../_includes/layouts",
+    data: "../_data",
+    output: "_site"
+  }
+}
+
