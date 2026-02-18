3bdca45857d417825222fe8789e548b115502540
kiki <142045045+calciferchang@users.noreply.github.com>
2026-02-17 21:17:34 -0500

    style & position nav

--- public/js/the-chair/carousel.js
+++ public/js/the-chair/carousel.js
@@ -14,6 +14,14 @@ const swiperV = new Swiper(".swiper-container-v", {
     nextEl: ".swiper-button-down",
     prevEl: ".swiper-button-up",
   },
+  on: {
+    slideChangeTransitionStart: function (e) {
+      setChapter(swiperV.activeIndex);
+    },
+    init: function (e) {
+      setChapter(this.activeIndex);
+    },
+  },
 });
 
 const swiperH = new Swiper(".swiper-container-h", {
@@ -34,4 +42,29 @@ const swiperH = new Swiper(".swiper-container-h", {
   },
   slidesPerView: 1.1,
   centeredSlides: true,
+});
+
+function setChapter(indx) {
+  indx++;
+
+  const nav = document.querySelector(".section-navigation");
+
+  // Remove 'current' from all items
+  nav.querySelectorAll("li.current").forEach((item) => {
+    item.classList.remove("current");
+  });
+
+  // Add 'current' to specific item
+  nav.querySelector("li:nth-child(" + indx + ")").classList.add("current");
+}
+
+document.querySelectorAll(".section-navigation > li").forEach(function (item) {
+  item.addEventListener("click", function () {
+    const allItems = Array.from(
+      document.querySelectorAll(".section-navigation > li"),
+    );
+    const index = allItems.indexOf(this);
+
+    swiperV.slideTo(index);
+  });
 });
